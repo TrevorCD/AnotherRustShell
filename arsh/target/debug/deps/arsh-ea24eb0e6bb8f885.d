37194633f49249567e38d2198939b628e16bb977
TrevorCD <trevorcldrwd@gmail.com>
2025-07-13 17:33:07 -0700

    more cargo files I don't yet understand

---
+++ arsh/target/debug/deps/arsh-ea24eb0e6bb8f885.d
@@ -0,0 +1,5 @@
+/home/t/Projects/AnotherRustShell/arsh/target/debug/deps/arsh-ea24eb0e6bb8f885.d: src/main.rs
+
+/home/t/Projects/AnotherRustShell/arsh/target/debug/deps/arsh-ea24eb0e6bb8f885: src/main.rs
+
+src/main.rs:
